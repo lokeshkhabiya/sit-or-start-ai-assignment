c9302b0ac7a238e3f9a59e91d3dfd25008f427ce
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2026-02-10 21:49:22 +0530

    feat: v0 complete

--- apps/web/src/utils/api-connection.ts
+++ apps/web/src/utils/api-connection.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+// export const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+export const BASE_URL = "https://assignmentserver.lokeshh.com"
